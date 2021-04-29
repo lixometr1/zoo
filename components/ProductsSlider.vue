973044224fd61e4f4c9adcf123455557a32b2ffe
Yaroslav <lixometr@mail.ru>
2021-04-29 10:14:12 +0300

    fix arrows

--- components/ProductsSlider.vue
+++ components/ProductsSlider.vue
@@ -6,21 +6,26 @@
     <client-only>
       <swiper class="products-slider__slider" :options="sliderOpts">
         <swiper-slide v-for="(product, idx) in 10" :key="idx">
-          <ProductCard :showBtn="false" :showLabels="false" size="sm" :multilineTitle="true" />
+          <ProductCard
+            :showBtn="false"
+            :showLabels="false"
+            size="sm"
+            :multilineTitle="true"
+          />
         </swiper-slide>
-        <div
-          slot="button-next"
-          class="swiper-button-next products-slider__arrow products-slider__arrow-next circle-arrow swiper-button-reset"
-        >
-          <svgArrowRight width="10" />
-        </div>
-        <div
-          slot="button-prev"
-          class="swiper-button-prev products-slider__arrow products-slider__arrow-prev circle-arrow swiper-button-reset"
-        >
-          <svgArrowLeft width="10" />
-        </div>
       </swiper>
+      <div
+        :id="'swiper-button-next-' + _uid"
+        class="swiper-button-next products-slider__arrow products-slider__arrow-next circle-arrow swiper-button-reset"
+      >
+        <svgArrowRight width="10" />
+      </div>
+      <div
+        :id="'swiper-button-prev-' + _uid"
+        class="swiper-button-prev products-slider__arrow products-slider__arrow-prev circle-arrow swiper-button-reset"
+      >
+        <svgArrowLeft width="10" />
+      </div>
     </client-only>
   </div>
 </template>
@@ -40,33 +45,35 @@ export default defineComponent({
       default: () => [],
     },
   },
-  setup() {
-    const sliderOpts: SwiperOptions = {
-      slidesPerView: 2,
-      threshold: 5,
-      breakpoints: {
-        1450: {
-          slidesPerView: 7,
+  computed: {
+    sliderOpts() {
+      const sliderOpts: SwiperOptions = {
+        slidesPerView: 2,
+        threshold: 5,
+        breakpoints: {
+          1450: {
+            slidesPerView: 7,
+          },
+          1300: {
+            slidesPerView: 6,
+          },
+          1100: {
+            slidesPerView: 5,
+          },
+          990: {
+            slidesPerView: 4,
+          },
+          760: {
+            slidesPerView: 3,
+          },
         },
-        1300: {
-          slidesPerView: 6,
+        navigation: {
+          nextEl: '#swiper-button-next-' + this._uid,
+          prevEl: '#swiper-button-prev-' + this._uid,
         },
-        1100: {
-          slidesPerView: 5,
-        },
-        990: {
-          slidesPerView: 4,
-        },
-        760: {
-          slidesPerView: 3,
-        },
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    }
-    return { sliderOpts }
+      }
+      return sliderOpts
+    },
   },
 })
 </script>
@@ -77,7 +84,7 @@ export default defineComponent({
   &__title {
     @apply lg:absolute lg:top-[40px] lg:pr-[140px] lg:w-full lg:transform lg:-translate-y-1/2 sm:top-[28px]  sm:pr-[110px];
   }
-  .circle-arrow {
+  &__arrow {
     @apply lg:top-[10px] lg:mt-0;
     svg {
       @apply sm:w-[8px];
@@ -86,8 +93,24 @@ export default defineComponent({
   &__slider {
     @apply lg:pt-[80px] sm:pt-[60px];
   }
+  &__arrow-next {
+    @apply transform translate-x-full -right-5 lg:right-0 lg:translate-x-0;
+
+    @media screen and (max-width: 1680px) {
+      right: 20px;
+      transform: translate(0);
+    }
+  }
   &__arrow-prev {
-    @apply lg:right-[68px] lg:left-auto sm:right-[52px];
+    @apply -left-5 transform -translate-x-full lg:right-[80px] sm:right-[62px];
+    @media screen and (max-width: 1680px) {
+      left: 20px;
+      transform: translate(0);
+    }
+    @screen lg {
+      left: auto;
+      /* transform: translate(-50%); */
+    }
   }
 }
 </style>
